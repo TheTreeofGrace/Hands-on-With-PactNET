7c6226d6cd84372135cf659047eb6038ed660751
GraceATree <grace.tree@ecs-digital.co.uk>
2022-08-16 09:03:12 +0100

    breaking

---
+++ Pact.Consumer/Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Pact.Consumer;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Pact.Consumer/Utils/MockProvider.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using PactNet;
+using PactNet.Infrastructure.Outputters;
+using Xunit.Abstractions;
+
+namespace Pact.Consumer.Utils;
+
+public class MockProvider
+{
+    private const int MockServerPort = 1234;
+    protected readonly IPactBuilderV3 MockProviderServer;
+
+    protected MockProvider(ITestOutputHelper output)
+    {
+        var config = new PactConfig
+        {
+            PactDir = Path.Join(@"../../../../../", "pact/pacts"),
+            DefaultJsonSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            },
+            Outputters = new List<IOutput>
+            {
+                new XUnitOutput(output)
+            },
+        };
+
+        MockProviderServer = PactNet.Pact.V3("ApiClient", "ProductService", config).UsingNativeBackend(MockServerPort);
+    }
+}
